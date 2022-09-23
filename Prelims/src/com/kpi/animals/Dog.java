633043abe545eb0137c4511ed90b72d7e159efb8
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-09-23 17:50:55 +0530

    git2

---
+++ Prelims/src/com/kpi/animals/Dog.java
@@ -0,0 +1,9 @@
+package com.kpi.animals;
+
+//public class Dog extends Animal
+//dog is a omnivorous : is a relationship is inhertance....which try to
+//child class= dog, parent class=omnivorous
+//extends = bind child to parent
+public class Dog extends Omnivorous {
+
+}
